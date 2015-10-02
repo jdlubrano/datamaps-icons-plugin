1730caf0ed282c2f6c54f71e828c511f6998e6b0
Joel Lubrano <jlubrano@lexmark.com>
2015-10-02 14:07:17 -0400

    All tests passing for hover events!

--- tests/spec/HoverEventsSpec.js
+++ tests/spec/HoverEventsSpec.js
@@ -66,11 +66,76 @@ describe('icons plugin - hover events', function() {
     expect(hoverOverSpy).not.toHaveBeenCalled();
   });
 
+  it('should pass the icon svg element as the context (i.e. this)', function() {
+    moveMouseOverIcon($icons[0]);
+    expect(hoverOverSpy.calls.first().object).toBe($icons[0]);
+    moveMouseOutOfIcon($icons[0]);
+    expect(hoverOutSpy.calls.first().object).toBe($icons[0]);
+  });
+
   it('should pass the data and the index as arguments', function() {
     moveMouseOverIcon($icons[0]);
     expect(hoverOverSpy).toHaveBeenCalledWith(TEST_DATA[0], 0);
     moveMouseOutOfIcon($icons[1]);
     expect(hoverOutSpy).toHaveBeenCalledWith(TEST_DATA[1], 1);
+  });
+
+  it('should apply the hover-over css class by default', function() {
+    moveMouseOverIcon($icons[0]);
+    var icon = d3.select($icons[0]);
+    expect(icon.classed('hover-over')).toBe(true);
+    expect(icon.classed('hover-out')).toBe(false);
+  });
+
+  it('should apply the hover-out css class by default', function() {
+    moveMouseOutOfIcon($icons[0]);
+    var icon = d3.select($icons[0]);
+    expect(icon.classed('hover-over')).toBe(false);
+    expect(icon.classed('hover-out')).toBe(true);
+  });
+
+  it('should allow the hover-over class to be set via options.hover.overClass', function() {
+    var opts = {
+      hover: {
+        overClass: 'hover_over_test'
+      }
+    };
+    dm.icons(TEST_DATA, opts);
+    moveMouseOverIcon($icons[0]);
+    var icon = d3.select($icons[0]);
+    expect(icon.classed(opts.hover.overClass)).toBe(true);        
+  });
+
+  it('should allow the hover-out class to be set via options.hover.outClass', function() {
+    var opts = {
+      hover: {
+        outClass: 'hover_out_class'
+      }
+    };
+    dm.icons(TEST_DATA, opts);
+    moveMouseOutOfIcon($icons[0]);
+    var icon = d3.select($icons[0]);
+    expect(icon.classed(opts.hover.outClass)).toBe(true);
+  });
+
+  it('css classes in the data level should override those in the options level', function() {
+    TEST_DATA[0].hover = {
+      overClass: 'foo'
+    };
+    TEST_DATA[1].hover = {
+      outClass: 'bar'
+    };
+    dm.icons(TEST_DATA, TEST_OPTS);
+    var first = d3.select($icons[0]);
+    var second = d3.select($icons[1]);
+    moveMouseOverIcon($icons[0]);
+    expect(first.classed('foo')).toBe(true);
+    moveMouseOutOfIcon($icons[0]);
+    expect(first.classed('hover-out')).toBe(true);
+    moveMouseOutOfIcon($icons[1]);
+    expect(second.classed('bar')).toBe(true);
+    moveMouseOverIcon($icons[1]);
+    expect(second.classed('hover-over')).toBe(true);
   });
 
 });
