dc8ebeff2a3940c95031d6bbb032855ccc9ec31b
Joel Lubrano <jlubrano@lexmark.com>
2015-10-28 13:55:21 -0400

    Bump version to 0.2.3

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 
 Package.describe({
   summary: "A plugin to the Datamaps library that provides support for overlaying svg icons.",
-  version: "0.2.2",
+  version: "0.2.3",
   name: "jdlubrano:datamaps-icons",
   git: "https://github.com/jdlubrano/datamaps-icons.git"
 });
