c9e15b3d93e2aae02e47bc7ccac7fd2ec720cc32
Joel Lubrano <joel@maketime.io>
2018-01-22 14:57:50 -0500

    Bump version

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 
 Package.describe({
   summary: "A plugin to the Datamaps library that provides support for overlaying svg icons.",
-  version: "0.2.6",
+  version: "0.2.7",
   name: "jdlubrano:datamaps-icons",
   git: "https://github.com/jdlubrano/datamaps-icons.git"
 });
